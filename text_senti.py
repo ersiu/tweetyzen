c43e677528abe393a181039d4378f9b23894e4dc
Eric Siu <eric.ay.siu@gmail.com>
2018-01-25 22:44:07 +0800

    version just implemented Vader, before removing own classifer

--- text_senti.py
+++ text_senti.py
@@ -1,7 +1,94 @@
+import re
+
+def set_topic(tweet, kwords1, kwords2):
+    text = tweet['text']
+    l_tags = tweet['entities']['hashtags']
+    tags = []
+    for d in l_tags:
+        h = d.get('text')
+        tags.append(h)
+
+    # hash tags
+    hashtags = ' '.join(tags)
+    #print('hashtags: ', hashtags)
+
+    score_t1 = calc_word_score(text, hashtags, kwords1)
+    score_t2 = calc_word_score(text, hashtags, kwords2)
+
+    if score_t1 > score_t2:
+        return 1
+    elif score_t2 > score_t1:
+        return 2
+    elif score_t1 == 0 and score_t2 == 0:
+        return 4
+    elif score_t1 == score_t2:
+        return 3
 
 
-#------------------------------------------------------------------------------
-import spacy
-from spacy.lemmatizer import Lemmatizer
-from spacy.lang.en.stop_words import STOP_WORDS
-nlp = spacy.load('en')
+#-------------------------------------------------------------------------------
+
+
+def calc_word_score(text, hashtags, lstkeywords):
+    keywords = lstkeywords.lower().split(',')
+    score = 0
+    for word in keywords:
+        word = word.lower().strip()
+        score += text.lower().count(word)
+        score += hashtags.lower().count(word)
+
+    return score
+
+
+#-------------------------------------------------------------------------------
+
+
+def clean_text(text):
+    text = re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", text).strip()
+    if text.startswith('RT'):
+        text = text.replace('RT', '', 1)
+    return text.strip()
+
+#-------------------------------------------------------------------------------
+
+def get_hashtags(data):
+    l_tags = row['entities']
+    tags = []
+    for d in l_tags:
+        h = d.get('text')
+        tags.append(h)
+    return ' '.join(tags)
+
+#-------------------------------------------------------------------------------
+
+
+def spacy_tokenizer(sentence, nlp):
+    tokens = nlp(sentence)
+    tokens = [tok.lemma_.lower().strip() if tok.lemma_ != "-PRON-" else tok.lower_ for tok in tokens]
+    tokens = [tok for tok in tokens if (tok not in STOP_WORDS and tok not in string.punctuation)]
+    return tokens
+
+#-------------------------------------------------------------------------------
+
+
+def calc_sentiment(text, myclassifier, vectorizer):
+    v_text = vectorizer.transform([text])
+    ans = myclassifier.predict_proba(v_text)
+    result = float(ans[0][1]) - float(ans[0][0])
+    return adjustSentiment(result)
+
+#-------------------------------------------------------------------------------
+
+def adjustSentiment(v):
+    if v < -0.35:
+        v += (v * 0.5)
+        if v < -1.0:
+            v = -1.0
+
+    if v > 0.35:
+        v += v * 0.5
+        if v > 1.0:
+            v = 1.0
+
+    return v
+
+#-------------------------------------------------------------------------------
--- zenmain.py
+++ zenmain.py
@@ -1,6 +1,7 @@
 import os
 import sys
-
+#------------------------------------------------------------------------------
+import text_senti
 #------------------------------------------------------------------------------
 import tweepy
 from tweepy import Stream
@@ -13,118 +14,16 @@ import string
 import json
 import _pickle as pickle
 import random
-import re
 #------------------------------------------------------------------------------
 import spacy
 from spacy.lemmatizer import Lemmatizer
 from spacy.lang.en.stop_words import STOP_WORDS
-
-import _pickle as pickle
-import random
-import re
-
-
+from sklearn.feature_extraction.text import TfidfVectorizer
 #------------------------------------------------------------------------------
 import configparser
 import argparse
-#-------------------------------------------------------------------------------
-
-
-def set_topic(tweet):
-    text = tweet['text']
-    l_tags = tweet['entities']['hashtags']
-    tags = []
-    for d in l_tags:
-        h = d.get('text')
-        tags.append(h)
-
-    # hash tags
-    hashtags = ' '.join(tags)
-    #print('hashtags: ', hashtags)
-
-    score_t1 = calc_word_score(text, hashtags, c_keywords1)
-    score_t2 = calc_word_score(text, hashtags, c_keywords2)
-    #print('score t1: ', score_t1)
-    #print('score t2: ', score_t2)
-
-    if score_t1 > score_t2:
-        return 1
-    elif score_t2 > score_t1:
-        return 2
-    elif score_t1 == 0 and score_t2 == 0:
-        return 4
-    elif score_t1 == score_t2:
-        return 3
-
-#-------------------------------------------------------------------------------
-# create 'topic' column if keyword is found and assign values
-
-
-def calc_word_score(text, hashtags, lstkeywords):
-    keywords = lstkeywords.lower().split(',')
-    score = 0
-    for word in keywords:
-        word = word.lower().strip()
-        score += text.lower().count(word)
-        score += hashtags.lower().count(word)
-
-    return score
-
-#-------------------------------------------------------------------------------
-
-
-def clean_text(text):
-    text = re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", text).strip()
-    if text.startswith('RT'):
-        text = text.replace('RT', '', 1)
-    return text.strip()
-
-#-------------------------------------------------------------------------------
-
-
-def get_hashtags(data):
-    l_tags = row['entities']
-    tags = []
-    for d in l_tags:
-        h = d.get('text')
-        tags.append(h)
-    return ' '.join(tags)
-
-#-------------------------------------------------------------------------------
-
-
-def spacy_tokenizer(sentence):
-    tokens = nlp(sentence)
-    tokens = [tok.lemma_.lower().strip() if tok.lemma_ != "-PRON-" else tok.lower_ for tok in tokens]
-    tokens = [tok for tok in tokens if (tok not in STOP_WORDS and tok not in string.punctuation)]
-    return tokens
-
-#-------------------------------------------------------------------------------
-
-
-def calc_sentiment(text, myclassifier):
-    v_text = vectorizer.transform([text])
-    ans = myclassifier.predict_proba(v_text)
-    result = float(ans[0][1]) - float(ans[0][0])
-    return adjustSentiment(result)
-
-#-------------------------------------------------------------------------------
-
-
-def adjustSentiment(v):
-    if v < -0.35:
-        v += (v * 0.5)
-        if v < -1.0:
-            v = -1.0
-
-    if v > 0.35:
-        v += v * 0.5
-        if v > 1.0:
-            v = 1.0
-
-    return v
-
-#-------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 
 def format_filename(fname):
@@ -133,13 +32,24 @@ def format_filename(fname):
 
 #-------------------------------------------------------------------------------
 
-
 def convert_valid(one_char):
     valid_chars = "-_.%s%s" % (string.ascii_letters, string.digits)
     if one_char in valid_chars:
         return one_char
     else:
         return '_'
+#-------------------------------------------------------------------------------
+
+def spacy_tokenizer(sentence):
+    tokens = nlp(sentence)
+    tokens = [tok.lemma_.lower().strip() if tok.lemma_ != "-PRON-" else tok.lower_ for tok in tokens]
+    tokens = [tok for tok in tokens if (tok not in STOP_WORDS and tok not in string.punctuation)]
+    return tokens
+#-------------------------------------------------------------------------------
+
+def vader_sentiment_scores(sentence):
+    snt = analyser.polarity_scores(sentence)
+    return (snt['compound'])
 
 #-------------------------------------------------------------------------------
 
@@ -165,33 +75,36 @@ class MyListener(StreamListener):
             tweet = json.loads(data)
             text = tweet['text']
             print('text: ', text)
-            
+
 
             if B_CALC_SENTI:
-                clean = clean_text(text)
-                sent_val = calc_sentiment(clean, mnb)
-                sent_val = float("{0:.2f}".format(sent_val))
+                # clean = text_senti.clean_text(text)
+                # sent_val = text_senti.calc_sentiment(clean, mnb, vect)
+                # sent_val = float("{0:.2f}".format(sent_val))
+
+                sent_val = vader_sentiment_scores(text)
                 tweet['senti_value'] = sent_val
-            
                 print('senti_value: ', sent_val)
-            
+
+
+
                 if B_CHECK_SIDE:
-                    side = set_topic(tweet)
+                    side = text_senti.set_topic(tweet, c_keywords1, c_keywords2)
                     print('side: ', side)
                     tweet['side'] = side
 
-            
+
             s = json.dumps(tweet)
             s = s + '\n'
-            
+
             if not os.access(self.outfile, os.W_OK): time.sleep(0.5)
-                
+
             # write to outfile
             with open(self.outfile, 'a') as f:
                 f.write(s)
-            
+
             live_output = ''
-            
+
             if B_LIVECHART:
 
                 if side == 1:
@@ -242,7 +155,7 @@ if __name__ == '__main__':
     args = vars(parser.parse_args())
 
     c_config_file = ''
-    
+
     if args['config']:
          c_config_file = args['config']
          print(">>>>> Load config file: ", c_config_file)
@@ -259,17 +172,17 @@ if __name__ == '__main__':
     c_consumer_secret = config.get("account", "consumer_secret")
     c_access_token = config.get("account", "access_token")
     c_access_secret = config.get("account", "access_secret")
-    
-    c_calc_senti = config.get("general", "calc_senti")
-    c_check_side = config.get("general", "check_side")
+
+    c_calc_senti = int(config.get("general", "calc_senti"))
+    c_check_side = int(config.get("general", "check_side"))
     c_save_dir = config.get("general", "save_dir")
     c_mode = config.get("general", "mode")
-    c_livechart = config.get("general", "livechart")
+    c_livechart = int(config.get("general", "livechart"))
     c_live_out_file = config.get("general", "live_out_file")
 
     c_mnb_file = config.get("algo", "mnb_file")
     c_vect_file = config.get("algo", "vect_file")
-    
+
     c_no_of_topic = int(config.get("topic", "no_of_topic"))
     c_theme = config.get("topic", "theme")
     c_topic1 = config.get("topic", "topic1")
@@ -277,28 +190,34 @@ if __name__ == '__main__':
     c_track = config.get("topic", "track")
     c_keywords1 = config.get("topic", "keywords1")
     c_keywords2 = config.get("topic", "keywords2")
-    
+
     B_LIVECHART = B_CALC_SENTI = B_CHECK_SIDE = False
 
-    if c_livechart == '1':
-        B_LIVECHART = B_CALC_SENTI = B_CHECK_SIDE = True
+    if c_no_of_topic == 1:
+        print('ss')
+        if c_calc_senti == 1: B_CALC_SENTI = True
+
     else:
-        if c_calc_senti == '1':
-            B_CALC_SENTI = True
+        if c_livechart == 1:
+            B_LIVECHART = B_CALC_SENTI = B_CHECK_SIDE = True
         else:
-            B_CALC_SENTI = False
+            if c_calc_senti == 1: B_CALC_SENTI = True
+            if c_check_side == 1: B_CHECK_SIDE = True
 
-        if c_check_side == '1':
-            B_CHECK_SIDE = True
-        else:
-            B_CHECK_SIDE = False
+    print ('>>>>> c_no_of_topic:', c_no_of_topic)
+    print ('>>>>> B_LIVECHART:', B_LIVECHART)
+    print ('>>>>> B_CALC_SENTI:', B_CALC_SENTI)
+    print ('>>>>> B_CHECK_SIDE:', B_CHECK_SIDE)
 
-    if c_no_of_topic == '1':
-        B_CHECK_SIDE = False
 
     #-------------- finish config -----------------
 
+
+
     if B_CALC_SENTI:
+
+        analyser = SentimentIntensityAnalyzer()
+
         # open multinominal naive bayes classifer
         print('>>>>> Initialize: load pickle mnb file')
         open_file = open(c_mnb_file, "rb")
@@ -306,21 +225,19 @@ if __name__ == '__main__':
         open_file.close()
 
         # open tfidf vectorizer
-        print('>>>>> Initialize: load pickle mnb file')
+        print('>>>>> Initialize: load pickle vectorizer file')
         open_file = open(c_vect_file, "rb")
-        vectorizer = pickle.load(open_file)
+        vect = pickle.load(open_file)
         open_file.close()
 
-        print('>>>>> Initialize: load NLP spacy')
-        nlp = spacy.load('en')
+
+    print('>>>>> Initialize: load NLP spacy')
+    nlp = spacy.load('en')
 
     print('>>>>> Initialize: Twitter auth handler')
     auth = OAuthHandler(c_consumer_key, c_consumer_secret)
     auth.set_access_token(c_access_token, c_access_secret)
-    
-    print ('>>>>> B_LIVECHART:', B_LIVECHART)
-    print ('>>>>> B_CALC_SENTI:', B_CALC_SENTI)
-    print ('>>>>> B_CHECK_SIDE:', B_CHECK_SIDE)
+
 
     print('>>>>> Initialize: Ready to start tracking')
 
